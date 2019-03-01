b5e6847c6b29488d90aa44d9dc3b908108622cae
Alexis Paques <alexis.paques@gmail.com>
2019-03-01 14:54:22 +0100

    Fix some naming issues.

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     version='0.1.0',
     author='AlexisTM',
     author_email='alexis.paques@gmail.com',
-    packages=['multilateration', 'localization.test'],
+    packages=['multilateration'],
     scripts=[],
-    url='https://github.com/alexistm/Localization',
+    url='https://github.com/AlexisTM/Multilateration',
     license='LICENSE.txt',
-    description='Multilateration library for 3D setups.',
+    description='Multilateration library for 2D, 3D setups and later 2D5 (3D with some axis fixed).',
     long_description=open('README.md').read(),
 )
