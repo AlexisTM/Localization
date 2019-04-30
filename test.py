28891a6cf802cb01904cef8433ab47cc58685964
Alexis Paques <alexis.paques@gmail.com>
2019-04-30 14:14:19 +0200

    Fix typo on anchor name

--- test.py
+++ test.py
@@ -3,14 +3,14 @@ from math import sqrt
 
 P=multilateration.Project(goal=[None, None, 2.0])
 # P=multilateration.Project()
-P.add_anchor('anchore_A',(1,0,1))
-P.add_anchor('anchore_B',(-1,0,1))
-P.add_anchor('anchore_C',(1,1,1))
+P.add_anchor('anchor_A',(1,0,1))
+P.add_anchor('anchor_B',(-1,0,1))
+P.add_anchor('anchor_C',(1,1,1))
 
 t = P.add_target()
-t.add_measure('anchore_A',sqrt(2))
-t.add_measure('anchore_B',sqrt(2))
-t.add_measure('anchore_C',sqrt(3))
+t.add_measure('anchor_A',sqrt(2))
+t.add_measure('anchor_B',sqrt(2))
+t.add_measure('anchor_C',sqrt(3))
 P.solve()
 
 print(t.ID, str(t.position))
