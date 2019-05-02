65296c79ebc51c598b236638870850503e521dce
Alexis Paques <alexis.paques@gmail.com>
2019-05-02 11:19:23 +0200

    Pythonification, using Engine instead of Project and set it in another file

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
-import multilateration
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from multilateration import Engine
 from math import sqrt
 
-P=multilateration.Project(goal=[None, None, 2.0]) # To fix that the resulting height should be 2 meters
+engine = Engine(goal=[None, None, 2.0]) # To fix that the resulting height should be 2 meters
 # P=multilateration.Project() # for 3D, simply do not set a goal
-P.add_anchor('anchor_A',(1,0,1))
-P.add_anchor('anchor_B',(-1,0,1))
-P.add_anchor('anchor_C',(1,1,1))
+engine.add_anchor('anchor_A',(1,0,1))
+engine.add_anchor('anchor_B',(-1,0,1))
+engine.add_anchor('anchor_C',(1,1,1))
 
-P.add_measure_id('anchor_A',sqrt(2))
-P.add_measure_id('anchor_B',sqrt(2))
-P.add_measure_id('anchor_C',sqrt(3))
-print(P.solve())
+engine.add_measure_id('anchor_A',sqrt(2))
+engine.add_measure_id('anchor_B',sqrt(2))
+engine.add_measure_id('anchor_C',sqrt(3))
+print(engine.solve())
